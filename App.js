38424457b517b8b812ec0ae6cd30e69e1f824192
Mohamed Nour <alkhalafmuhamade@gmail.com>
2024-01-04 01:11:52 +0300

    changes to be committed:     modified:   App.js     modified:   src/screens/HomeScreen.js

--- App.js
+++ App.js
@@ -33,7 +33,6 @@ export default function App() {
   const navigationRef = useRef();
 
   useEffect(() => {
-    checkRatesAndNotify();
     registerBackgroundFetch();
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/globalStyles';
-// Import the function from notifications.js
-import { sendNotification } from '../../notifications'; // Adjust the path as necessary
-
-// Define the token and message
 
 const HomeScreen = ({ navigation }) => {
-  const testToken = 'ExponentPushToken[wLQ-10Gzi78lhHLEyM3BIy]';
-  const testMessage = 'This is a test notification from the system.';
-
-  // Call the function
-  sendNotification(testToken, testMessage)
-    .then(() => console.log('Notification sent successfully'))
-    .catch(error => console.error('Error sending notification:', error));
-
+  
   return (
     <View style={globalStyles.container}>
       <TouchableOpacity style={styles.item} onPress={() => navigation.navigate("ExchangeRates")}><Text style={styles.itemName}>Forex</Text></TouchableOpacity>
       <TouchableOpacity style={styles.item} onPress={() => navigation.navigate("StockQuote")}><Text style={styles.itemName}>Stocks</Text></TouchableOpacity>
-      <TouchableOpacity style={styles.item} onPress={() => sendNotification()}><Text style={styles.itemName}>Notification</Text></TouchableOpacity>
     </View>
   );
 };
